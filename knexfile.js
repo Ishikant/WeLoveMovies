d098535277c6a8c1f251d6e5d5ca041b2100bc84
ISHIKANT DUSOYE <110443129+Ishikant@users.noreply.github.com>
2023-06-03 02:21:10 -0400

    Complete qualified assignment, ...

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgres://eroennai:Q6_7BXNFaceV87iX3T2yBhquuhVNizpd@rajje.db.elephantsql.com/eroennai",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
